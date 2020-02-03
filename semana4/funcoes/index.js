54978bd469af0bb45482ffd479e8479ce2f2630a
Nadia Carvalho <carvalho.nadiacaroline@gmail.com>
2020-02-03 16:47:11 -0300

    entrega incompleta (ate o ex 4)

---
+++ semana4/funcoes/index.js
@@ -0,0 +1,41 @@
+/* Exercício 1
+a)[]
+b)(6) [0, 1, 0, 1, 2, 3]
+c)(12) [0, 1, 0, 1, 2, 3, 0, 1, 2, 3, 4, 5]
+*/
+
+/* Exercício 2
+a) 0, 2, undefined
+b)Sim. A função depende de o item buscado estar dentro do array ou não, e independe do tipo de array.
+*/
+/* Exercício 3
+A função multiplica ou adiciona os itens do array, dependendo do método (A ou B).
+Um melhor nome seria addOrMultiply
+*/
+/* Exercício 4 */
+
+// a)
+function dogyears(year) {
+   return year * 7 
+}
+
+// b) 
+function personalData(name, age, address, isStudent) {
+  let message = "Eu sou " + name + ", tenho " + age + " anos, moro em " + address + " e "
+  if (isStudent) {
+    message += "sou"
+  } else {
+    message += "não sou"
+  }
+  message += " estudante"
+  return message
+}
+
+// Exercício 5
+
+/* Exercício 6
+a)
+b)
+c)
+d)
+*/
