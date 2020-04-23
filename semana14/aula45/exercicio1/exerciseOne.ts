d18909ca1bd058f29bce6e53fa5f645ec07b57ae
Nadia Carvalho <carvalho.nadiacaroline@gmail.com>
2020-04-23 18:15:14 -0300

    entrega muito incompleta

---
+++ semana14/aula45/exercicio1/exerciseOne.ts
@@ -0,0 +1,18 @@
+import { readFile, readdir } from 'fs'; 
+
+readdir("./textos", (err: Error, files: string[],) => {
+  console.log(files)
+})
+
+// myPromise = new Promise((response, reject) => {
+//   const handleFileRead =  (err: Error, data: Buffer) => {
+//     try {
+
+//     } catch (e) {
+//       reject(err)
+//     }
+//   }
+
+
+
+// })
